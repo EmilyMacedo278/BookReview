ed04e4e6ffa54775afc165cc642fedd31f3ec891
EmilyMacedo278 <emily27289@Gmail.com>
2026-03-30 20:55:59 -0300

    Fazendo reajustes

--- BookReview/src/main/java/com/example/BookReview/models/Book.java
+++ BookReview/src/main/java/com/example/BookReview/models/Book.java
@@ -4,8 +4,18 @@ import lombok.*;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 @Data
+@Entity
+
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Long id;
     private String titulo;
--- BookReview/src/main/java/com/example/BookReview/models/Review.java
+++ BookReview/src/main/java/com/example/BookReview/models/Review.java
@@ -4,11 +4,26 @@ import lombok.*;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
 @Data
+@Entity
 public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long nota;
+
+    private Integer nota;
     private String comentario;
     private LocalDate dataAvaliacao;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
     private Book book;
 }
